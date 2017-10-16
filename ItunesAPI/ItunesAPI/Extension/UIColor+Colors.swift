e06874bacd25755af63774c14e0c8ef713419f4f
Hyeonjin Jang-MBP <jhjang@huray.net>
2017-10-16 21:04:14 +0900

    [UIColor] 카카오 컬러 추가.

---
+++ ItunesAPI/ItunesAPI/Extension/UIColor+Colors.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Colors.swift
+//  ItunesAPI
+//
+//  Created by Hyeonjin Jang on 2017. 10. 16..
+//  Copyright © 2017년 KakaoBank. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  class var kakaoColor: UIColor {
+    return UIColor.init(red: 250/255.0, green: 225/255.0, blue: 0, alpha: 1)
+  }
+}
